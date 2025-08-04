43ec4e6fe92c7bf2fb130bc86d201f3ff7663640
Gliger13 <zanevskiyandrey@gmail.com>
2025-08-04 20:40:27 +0200

    feat: add cli colors

--- vrchat_osc_scripts/launcher.py
+++ vrchat_osc_scripts/launcher.py
@@ -1,7 +1,15 @@
 """Simple script with error handling to prevent the .exe from exiting immediately on error."""
 
 try:
+    import os
+
     import main
+
+    # Strange windows thing to support console colors
+    # Ignore Bandit warnings since we spawn the simplest possible process
+    # to clear the just-created Command Prompt window.
+    # B605: Starting a process with a shell. B607: Starting a process with a partial executable path
+    os.system("cls")  # nosec B605, B607
 
     main.main()
 except Exception:
